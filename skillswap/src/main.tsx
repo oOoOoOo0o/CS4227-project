13372661fb36cf6a119fec891687963d5aa3bf00
Bayan <b.nezamabad@gmail.com>
2025-03-15 21:20:56 +0000

    Navbar working

--- skillswap/src/main.tsx
+++ skillswap/src/main.tsx
@@ -3,10 +3,12 @@ import {createRoot} from "react-dom/client"
 import App from "./App.tsx"
 import {Provider} from "react-redux";
 import store from "./redux/store.tsx";
+import Navbar from "./components/Navbar.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
+            <Navbar/>
             <App/>
         </Provider>
     </StrictMode>,
