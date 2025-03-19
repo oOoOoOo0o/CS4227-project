66cbbb22bc2532e87c08fd1c59a7cbfdd71273fb
Bayan <b.nezamabad@gmail.com>
2025-03-19 12:42:42 +0000

    Tommy help b

---
+++ skillswap/src/database/firebase.tsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyATlpubqY6kn4n6LC4-dDo8K89EbLcExbk",
+    authDomain: "skillswap-e2e66.firebaseapp.com",
+    projectId: "skillswap-e2e66",
+    storageBucket: "skillswap-e2e66.firebasestorage.app",
+    messagingSenderId: "457743034878",
+    appId: "1:457743034878:web:0bae53d2fe8fa2260f38af",
+    measurementId: "G-64SXHRF89L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
